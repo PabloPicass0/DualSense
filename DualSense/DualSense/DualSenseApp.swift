91bb919686e3ef872d82d11a09b035b19610f13c
Pablo Behrens <pablobehrens@dyn3158-74.wlan.ic.ac.uk>
2023-06-13 15:44:01 +0100

    Initial xCode setup

---
+++ DualSense/DualSense/DualSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DualSenseApp.swift
+//  DualSense
+//
+//  Created by Pablo Behrens on 13.06.23.
+//
+
+import SwiftUI
+
+@main
+struct DualSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
